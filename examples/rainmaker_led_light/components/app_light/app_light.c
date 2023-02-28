a857bab7a73fed9c9088d6b899355e326bbf1569
Tian Sen Wen <tiansenwen@espressif.com>
2023-02-28 22:44:50 +0800

    feat: support esp32-c2 & Updated litemesh core version to 4a6dd88d
    - feature: support esp32-c2
    - fix: API compatible with idf5.x and idf4.x versions

--- examples/rainmaker_led_light/components/app_light/app_light.c
+++ examples/rainmaker_led_light/components/app_light/app_light.c
@@ -13,8 +13,10 @@
 #include <esp_rmaker_standard_devices.h>
 
 #include <app_reset.h>
-#include <ws2812_led.h>
 #include <app_light.h>
+#include <lightbulb.h>
+
+static const char *TAG = "app_light";
 
 /* This is the button that is used for toggling the power */
 #define BUTTON_GPIO          CONFIG_EXAMPLE_BOARD_BUTTON_GPIO
@@ -39,16 +41,16 @@ esp_err_t app_light_set_led(uint32_t hue, uint32_t saturation, uint32_t brightne
                 esp_rmaker_device_get_param_by_type(light_device, ESP_RMAKER_PARAM_POWER),
                 esp_rmaker_bool(g_power));
     }
-    return ws2812_led_set_hsv(hue, saturation, brightness);
+    return lightbulb_set_hsv(hue, saturation, brightness);
 }
 
 esp_err_t app_light_set_power(bool power)
 {
     g_power = power;
     if (power) {
-        ws2812_led_set_hsv(g_hue, g_saturation, g_value);
+        lightbulb_set_hsv(g_hue, g_saturation, g_value);
     } else {
-        ws2812_led_clear();
+        lightbulb_set_hsv(0, 0, 0);
     }
     return ESP_OK;
 }
@@ -73,14 +75,76 @@ esp_err_t app_light_set_saturation(uint16_t saturation)
 
 esp_err_t app_light_init(void)
 {
-    esp_err_t err = ws2812_led_init();
+    lightbulb_config_t config = {
+        //1. Select and configure the chip
+#ifdef CONFIG_LIGHTBULB_DEMO_DRIVER_SELECT_WS2812
+        .type = DRIVER_WS2812,
+        .driver_conf.ws2812.led_num = CONFIG_WS2812_LED_NUM,
+        .driver_conf.ws2812.ctrl_io = CONFIG_WS2812_LED_GPIO,
+#endif
+#ifdef CONFIG_LIGHTBULB_DEMO_DRIVER_SELECT_PWM
+        .type = DRIVER_ESP_PWM,
+        .driver_conf.pwm.freq_hz = CONFIG_PWM_FREQ_HZ,
+#ifdef CONFIG_IDF_TARGET_ESP32C2
+        /* Adapt to ESP8684-DevKitM-1 
+         * For details, please refer to: 
+         * https://docs.espressif.com/projects/espressif-esp-dev-kits/zh_CN/latest/esp8684/esp8684-devkitm-1/user_guide.html
+        */
+        .driver_conf.pwm.invert_level = true,
+#endif
+#endif
+#ifdef CONFIG_LIGHTBULB_DEMO_DRIVER_SELECT_SM2135E
+        .type = DRIVER_SM2135E,
+        .driver_conf.sm2135e.freq_khz = 400,
+        .driver_conf.sm2135e.enable_iic_queue = true,
+        .driver_conf.sm2135e.iic_clk = CONFIG_SM2135E_IIC_CLK_GPIO,
+        .driver_conf.sm2135e.iic_sda = CONFIG_SM2135E_IIC_SDA_GPIO,
+        .driver_conf.sm2135e.rgb_current = SM2135E_RGB_CURRENT_20MA,
+        .driver_conf.sm2135e.wy_current = SM2135E_WY_CURRENT_40MA,
+#endif
+        // 2. Configure the drive capability
+        .capability.enable_fades = false,
+        .capability.fades_ms = 800,
+        .capability.enable_status_storage = false,
+        .capability.mode_mask = COLOR_MODE,
+        .capability.storage_cb = NULL,
+
+        //3. Configure driver io
+#ifdef CONFIG_LIGHTBULB_DEMO_DRIVER_SELECT_PWM
+        .io_conf.pwm_io.red = CONFIG_PWM_RED_GPIO,
+        .io_conf.pwm_io.green = CONFIG_PWM_GREEN_GPIO,
+        .io_conf.pwm_io.blue = CONFIG_PWM_BLUE_GPIO,
+#endif
+#ifdef CONFIG_LIGHTBULB_DEMO_DRIVER_SELECT_SM2135E
+        .io_conf.iic_io.red = OUT3,
+        .io_conf.iic_io.green = OUT2,
+        .io_conf.iic_io.blue = OUT1,
+        .io_conf.iic_io.cold_white = OUT5,
+        .io_conf.iic_io.warm_yellow = OUT4,
+#endif
+        //4. Limit param
+        .external_limit = NULL,
+
+        //5. Gamma param
+        .gamma_conf = NULL,
+
+        //6. Init param
+        .init_status.mode = WORK_COLOR,
+        .init_status.on = true,
+        .init_status.hue = 0,
+        .init_status.saturation = 100,
+        .init_status.value = 100,
+    };
+    esp_err_t err = lightbulb_init(&config);
     if (err != ESP_OK) {
+        ESP_LOGE(TAG, "There may be some errors in the configuration, please check the log.");
         return err;
     }
+
     if (g_power) {
-        ws2812_led_set_hsv(g_hue, g_saturation, g_value);
+        lightbulb_set_hsv(g_hue, g_saturation, g_value);
     } else {
-        ws2812_led_clear();
+        lightbulb_set_hsv(0, 0, 0);
     }
     return ESP_OK;
 }
