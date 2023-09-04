espressif/esp-mesh-lite

commit 1da9de89095d8ab73da905aa8d48b812cb9cac6e

feat(core): Updated litemesh core version to 5a978158
feat: add join mesh without configured wifi
fix: circular recovery issue caused by hard reboot
feat: Optimization for the "no_router" scenario.

Close:
https://github.com/espressif/esp-mesh-lite/issues/11
https://github.com/espressif/esp-mesh-lite/issues/12

components/mesh_lite/lib/VERSION
@@ -1,6 +1,6 @@
 version
-libesp_mesh_lite_esp32.a   :9348181
-libesp_mesh_lite_esp32c2.a :9348181
-libesp_mesh_lite_esp32c3.a :9348181
-libesp_mesh_lite_esp32s2.a :9348181
-libesp_mesh_lite_esp32s3.a :9348181
+libesp_mesh_lite_esp32.a   :5a97815
+libesp_mesh_lite_esp32c2.a :5a97815
+libesp_mesh_lite_esp32c3.a :5a97815
+libesp_mesh_lite_esp32s2.a :5a97815
+libesp_mesh_lite_esp32s3.a :5a97815

commit 8ef8aefdc533021f7dc4ab79c6262eea4cc76724

feat: Updated litemesh core version to 34377a9c
- feat: Add esp_mesh_lite_start API
- fix: Memory leak caused by 1da9de89
- chore(LAN OTA): Place esp_restart outside of the lib

components/mesh_lite/idf_component.yml
@@ -1,4 +1,4 @@
-version: "0.6.0"
+version: "0.7.0"
 description: A lite version Wi-Fi Mesh, each node can access the network over the IP layer.
 url: https://github.com/espressif/esp-mesh-lite/tree/master/components/mesh_lite
 repository: https://github.com/espressif/esp-mesh-lite.git

components/mesh_lite/include/esp_mesh_lite.h
@@ -37,6 +37,12 @@ typedef enum {
  */
 void esp_mesh_lite_init(esp_mesh_lite_config_t* config);
 
+/**
+ * @brief Start Mesh-Lite.
+ * 
+ */
+void esp_mesh_lite_start(void);
+
 typedef struct  esp_mesh_lite_node_info {
     uint8_t level;
     char mac[MAC_MAX_LEN];

components/mesh_lite/include/esp_mesh_lite_core.h
@@ -182,7 +182,8 @@ void esp_mesh_lite_set_mesh_id(uint8_t mesh_id, bool force_update_nvs);
 /**
  * @brief  Set which level this node is only allowed to be
  * 
- * @attention  Please make sure `esp_mesh_lite_init` is called before calling this function.
+ * @attention  1. Please call this API after  `esp_mesh_lite_init`
+ * @attention  2. Please call this API before `esp_mesh_lite_start`
  *
  * @param[in]  level: 1 ~ CONFIG_MESH_LITE_MAXIMUM_LEVEL_ALLOWED, 0 is invalid.
  * 
@@ -192,7 +193,8 @@ esp_err_t esp_mesh_lite_set_allowed_level(uint8_t level);
 /**
  * @brief  Set which level this node is not allowed to be used as
  * 
- * @attention  Please make sure `esp_mesh_lite_init` is called before calling this function.
+ * @attention  1. Please call this API after  `esp_mesh_lite_init`
+ * @attention  2. Please call this API before `esp_mesh_lite_start`
  *
  * @param[in]  level: 1 ~ CONFIG_MESH_LITE_MAXIMUM_LEVEL_ALLOWED, 0 is invalid.
  *
@@ -202,7 +204,8 @@ esp_err_t esp_mesh_lite_set_disallowed_level(uint8_t level);
 /**
  * @brief  Set router information
  * 
- * @attention  Please make sure `esp_mesh_lite_init` is called before calling this function.
+ * @attention  1. Please call this API after  `esp_mesh_lite_init`
+ * @attention  2. Please call this API before `esp_mesh_lite_start`
  *
  * @param[in]  conf
  *
@@ -212,7 +215,8 @@ esp_err_t esp_mesh_lite_set_router_config(wifi_sta_config_t *conf);
 /**
  * @brief  Whether to allow other nodes to join the mesh network.
  * 
- * @attention  Please make sure `esp_mesh_lite_init` is called before calling this function.
+ * @attention  1. Please call this API after  `esp_mesh_lite_init`
+ * @attention  2. Please call this API before `esp_mesh_lite_start`
  *
  * @param[in]  enable: true -> allow; false -> disallow
  *
@@ -222,7 +226,8 @@ esp_err_t esp_mesh_lite_allow_others_to_join(bool enable);
 /**
  * @brief  Set argot number.
  * 
- * @attention  Please make sure `esp_mesh_lite_init` is called before calling this function.
+ * @attention  1. Please call this API after  `esp_mesh_lite_init`
+ * @attention  2. Please call this API before `esp_mesh_lite_start`
  *
  * @param[in]  argot
  *

components/mesh_lite/lib/VERSION
@@ -1,6 +1,6 @@
 version
-libesp_mesh_lite_esp32.a   :8d3f016
-libesp_mesh_lite_esp32c2.a :8d3f016
-libesp_mesh_lite_esp32c3.a :8d3f016
-libesp_mesh_lite_esp32s2.a :8d3f016
-libesp_mesh_lite_esp32s3.a :8d3f016
+libesp_mesh_lite_esp32.a   :34377a9
+libesp_mesh_lite_esp32c2.a :34377a9
+libesp_mesh_lite_esp32c3.a :34377a9
+libesp_mesh_lite_esp32s2.a :34377a9
+libesp_mesh_lite_esp32s3.a :34377a9

components/mesh_lite/src/esp_mesh_lite.c
@@ -199,6 +199,7 @@ static void esp_mesh_lite_event_ip_changed_handler(void *arg, esp_event_base_t e
             break;
         case ESP_MESH_LITE_EVENT_OTA_SUCCESS:
             ESP_LOGI(TAG, "LAN OTA Success");
+            esp_restart();
             break;
     }
 }

examples/mesh_local_control/main/local_control.c
@@ -213,6 +213,8 @@ void app_main()
     esp_mesh_lite_config_t mesh_lite_config = ESP_MESH_LITE_DEFAULT_INIT();
     esp_mesh_lite_init(&mesh_lite_config);
 
+    esp_mesh_lite_start();
+
     /**
      * @breif Create handler
      */

examples/rainmaker_led_light/components/app_bridge/app_bridge.c
@@ -553,6 +553,8 @@ esp_err_t app_rmaker_enable_bridge(void)
     esp_mesh_lite_config_t mesh_lite_config = ESP_MESH_LITE_DEFAULT_INIT();
     esp_mesh_lite_init(&mesh_lite_config);
 
+    esp_mesh_lite_start();
+
     child_info_mutex = xSemaphoreCreateMutex();
 
     for (uint8_t i = 0; i < MAX_STATION; i++) {