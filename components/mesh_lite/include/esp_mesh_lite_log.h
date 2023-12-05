4801a53df2ea79cf3307701ee65c88ff4e0f7bdc
Tian Sen Wen <tiansenwen@espressif.com>
2023-12-05 16:09:32 +0800

    feat: add ESP_MESH_LITE_LOGx

---
+++ components/mesh_lite/include/esp_mesh_lite_log.h
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include "esp_err.h"
+#include "esp_log.h"
+
+#define ESP_MESH_LITE_LOG_LEVEL ESP_LOG_DEBUG
+
+void esp_mesh_lite_log_write(esp_log_level_t level, const char *tag, const char *format, ...);
+
+#define ESP_MESH_LITE_LOGE(format, ... ) do { \
+        if (ESP_MESH_LITE_LOG_LEVEL >= ESP_LOG_ERROR) { \
+            esp_mesh_lite_log_write(ESP_LOG_ERROR, TAG, format, ##__VA_ARGS__); \
+        } \
+    } while(0)
+
+#define ESP_MESH_LITE_LOGW(format, ... ) do { \
+        if (ESP_MESH_LITE_LOG_LEVEL >= ESP_LOG_WARN) { \
+            esp_mesh_lite_log_write(ESP_LOG_WARN, TAG, format, ##__VA_ARGS__); \
+        } \
+    } while(0)
+
+#define ESP_MESH_LITE_LOGI(format, ... ) do { \
+        if (ESP_MESH_LITE_LOG_LEVEL >= ESP_LOG_INFO) { \
+            esp_mesh_lite_log_write(ESP_LOG_INFO, TAG, format, ##__VA_ARGS__); \
+        } \
+    } while(0)
+
+#define ESP_MESH_LITE_LOGD(format, ... ) do { \
+        if (ESP_MESH_LITE_LOG_LEVEL >= ESP_LOG_DEBUG) { \
+            esp_mesh_lite_log_write(ESP_LOG_DEBUG, TAG, format, ##__VA_ARGS__); \
+        } \
+    } while(0)
+
+#define ESP_MESH_LITE_LOGV(format, ... ) do { \
+        if (ESP_MESH_LITE_LOG_LEVEL >= ESP_LOG_VERBOSE) { \
+            esp_mesh_lite_log_write(ESP_LOG_VERBOSE, TAG, format, ##__VA_ARGS__); \
+        } \
+    } while(0)
---
+++ components/mesh_lite/src/esp_mesh_lite_log.c
@@ -0,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "string.h"
+#include "inttypes.h"
+#include "esp_mesh_lite_log.h"
+
+#define LOG_COLOR_LEN    8
+
+void esp_mesh_lite_log_write(esp_log_level_t level, const char *tag, const char *format, ...)
+{
+    char letter = 'I';
+    char log_color[LOG_COLOR_LEN] = {0};
+    memset(log_color, 0, sizeof(log_color));
+    switch (level) {
+        case ESP_LOG_ERROR:
+            letter = 'E';
+#ifdef CONFIG_LOG_COLORS
+            memcpy(log_color, "\033[0;" "31" "m", LOG_COLOR_LEN);
+#endif
+            break;
+        case ESP_LOG_WARN:
+            letter = 'W';
+#ifdef CONFIG_LOG_COLORS
+            memcpy(log_color, "\033[0;" "33" "m", LOG_COLOR_LEN);
+#endif
+            break;
+        case ESP_LOG_INFO:
+            letter = 'I';
+#ifdef CONFIG_LOG_COLORS
+            memcpy(log_color, "\033[0;" "32" "m", LOG_COLOR_LEN);
+#endif
+            break;
+        case ESP_LOG_DEBUG:
+            letter = 'D';
+#ifdef CONFIG_LOG_COLORS
+            memcpy(log_color, "\033[0;" "36" "m", LOG_COLOR_LEN);
+#endif
+            break;
+        case ESP_LOG_VERBOSE:
+            letter = 'V';
+            break;
+        default:
+            break;
+    }
+    va_list list;
+    va_start(list, format);
+
+    size_t new_format_length = strlen(tag) + strlen(format) + 30;
+    char new_format[new_format_length];
+    snprintf(new_format, new_format_length, "%s%c (%"PRIu32") [%s]: %s " LOG_RESET_COLOR "\n", log_color, letter, esp_log_timestamp(), tag, format);
+
+    esp_log_writev(level, tag, new_format, list);
+    va_end(list);
+}
