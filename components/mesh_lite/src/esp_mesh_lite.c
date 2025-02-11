56d5ad2778d9dec1217434247fe0122fe7f960e6
Tian Sen Wen <tiansenwen@espressif.com>
2025-02-11 16:33:30 +0800

    chore: Remove 'mark app valid' inside the mesh lib

--- components/mesh_lite/src/esp_mesh_lite.c
+++ components/mesh_lite/src/esp_mesh_lite.c
@@ -415,4 +415,14 @@ void esp_mesh_lite_init(esp_mesh_lite_config_t* config)
 #if CONFIG_MESH_LITE_WIRELESS_DEBUG
     esp_mesh_lite_wireless_debug_init();
 #endif
+
+#if CONFIG_OTA_AUTO_CANCEL_ROLLBACK
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    esp_ota_img_states_t ota_state;
+    if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK) {
+        if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+            esp_ota_mark_app_valid_cancel_rollback();
+        }
+    }
+#endif
 }
