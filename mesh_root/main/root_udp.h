2e35a79df0d8508aa0952b58d9c2f2bdabb1a7ac
myreplit0-debug <myreplit0@gmail.com>
2025-09-15 21:11:40 +0100

    Update root_udp.h

--- mesh_root/main/root_udp.h
+++ mesh_root/main/root_udp.h
@@ -1,6 +1,3 @@
 #pragma once
 #include <stdint.h>
-
-// Start a UDP listener task that logs BLE lines
-// Takes the UDP port as argument
-void root_udp_start(uint16_t port);
+int root_udp_start(uint16_t port);
