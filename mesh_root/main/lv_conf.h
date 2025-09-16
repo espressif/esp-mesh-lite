5e6fdfae54b559a23f7b460e947f592ffbd383e6
myreplit0-debug <myreplit0@gmail.com>
2025-09-16 19:55:09 +0100

    Create lv_conf.h

---
+++ mesh_root/main/lv_conf.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* Tell LVGL to search for this simple config first */
+#define LV_CONF_INCLUDE_SIMPLE
+
+/* Use LVGL’s internal defaults so we don’t have to tune anything yet */
+#include "lvgl/lv_conf_internal.h"
