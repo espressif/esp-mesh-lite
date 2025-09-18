f0ada7445452d600a21d181f805971856d6d900a
myreplit0-debug <myreplit0@gmail.com>
2025-09-18 18:28:42 +0100

    Delete mesh_root/main/lv_conf.h

--- mesh_root/main/lv_conf.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/* Tell LVGL to search for this simple config first */
-#define LV_CONF_INCLUDE_SIMPLE
-
-/* Use LVGL’s internal defaults so we don’t have to tune anything yet */
-#include "lvgl/lv_conf_internal.h"
